8c076eb67671f1eae815ed87afdcefb66726e214
Doctor Wu <wuzhouchun@qq.com>
2026-03-13 16:42:23 +0800

    feat(slack): import runtime functions for Slack integration

--- packages/slack/src/index.ts
+++ packages/slack/src/index.ts
@@ -1,3 +1,5 @@
+import { isSlackRuntimeMainModule, startSlackRuntime } from './runtime.js';
+
 export { readSlackConfig, applySlackConfigEnvironment, resolveSlackConversationStateFile, resolveSlackPendingRunStateFile } from './config.js';
 export type { SlackConfig } from './config.js';
 export { buildSlackBackendSelectionBlocks, buildSlackModelSelectionBlocks } from './cards.js';
